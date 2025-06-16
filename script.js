2a994813e0917878ddb7df7ec36c3e42b6ae5141
FeedSnow <mateovip22262@gmail.com>
2025-06-16 14:29:22 +0200

    zad 1-5

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let clickCount = 0;
 
 const countryInput = document.getElementById('country');
+const countryCodeInput = document.getElementById('countryCode');
 const myForm = document.getElementById('form');
 const modal = document.getElementById('form-feedback-modal');
 const clicksInfo = document.getElementById('click-count');
@@ -12,13 +13,17 @@ function handleClick() {
 
 async function fetchAndFillCountries() {
     try {
-        const response = await fetch('https://restcountries.com/v3.1/all');
+        const response = await fetch('https://restcountries.com/v3.1/all?fields=name');
         if (!response.ok) {
             throw new Error('Błąd pobierania danych');
         }
         const data = await response.json();
-        const countries = data.map(country => country.name.common);
+        const countries = data.map(country => country.name.common).sort();
         countryInput.innerHTML = countries.map(country => `<option value="${country}">${country}</option>`).join('');
+
+        new TomSelect("#country");
+
+        getCountryByIP();
     } catch (error) {
         console.error('Wystąpił błąd:', error);
     }
@@ -29,7 +34,8 @@ function getCountryByIP() {
         .then(response => response.json())
         .then(data => {
             const country = data.country;
-            // TODO inject country to form and call getCountryCode(country) function
+            countryInput.value = country;
+            getCountryCode(country);
         })
         .catch(error => {
             console.error('Błąd pobierania danych z serwera GeoJS:', error);
@@ -48,7 +54,7 @@ function getCountryCode(countryName) {
     })
     .then(data => {        
         const countryCode = data[0].idd.root + data[0].idd.suffixes.join("")
-        // TODO inject countryCode to form
+        countryCodeInput.value = countryCode;
     })
     .catch(error => {
         console.error('Wystąpił błąd:', error);
